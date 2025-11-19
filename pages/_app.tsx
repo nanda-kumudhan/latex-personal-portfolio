5aceddaef20fbf784d69c818bdffda3e1f8e1b2b
Nanda Kumudhan <nandak97@protonmail.ch>
2025-11-19 01:22:36 +0000

    feat: add Vercel Analytics and Speed Insights components, update global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp;
